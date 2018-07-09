dfa8895d3ce551c854c59809ed7967ef9295d6e1
Hélio Silva <helio_almeida@hotmail.com>
2018-07-09 23:42:38 +0100

    Added database controller to pull items directly from database

---
+++ GoldenFeedbacksMaker/Controllers/DBController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoldenFeedbacksMaker.Controllers
+{
+    public class DBController : IDisposable
+    {
+        private const string connectionString = "Data Source=ROOMPC;Initial Catalog = GoldenFeedbacks; Integrated security = true";
+
+        SqlConnection sqlConnection { get; set; }
+
+        public DBController()
+        {
+            sqlConnection = new SqlConnection(connectionString);
+        }
+
+        public DataSet GetAllQuestions()
+        {
+            try
+            {
+                string commandText = @"SELECT Q.Number, Q.Question, Q.Answer, C.[Description] as Chapter, CM.Title, CM.Comment
+                                        FROM
+                                        tbQuestions Q
+                                        INNER JOIN tbChapters C ON Q.idChapter = C.id
+                                        LEFT JOIN tbComments CM ON C.id = CM.idQuestion
+                                        ";
+
+                sqlConnection.Open();
+
+                SqlDataAdapter adapter = new SqlDataAdapter(commandText, sqlConnection);
+
+                DataSet dsResult = new DataSet();
+                adapter.Fill(dsResult);
+
+                sqlConnection.Close();
+
+                return dsResult;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+            }
+        }
+    }
+}
--- GoldenFeedbacksMaker/Program.cs
+++ GoldenFeedbacksMaker/Program.cs
@@ -1,9 +1,11 @@
 ﻿using GoldenFeedbacksMaker.Classes;
+using GoldenFeedbacksMaker.Controllers;
 using GoldenFeedbacksMaker.Helpers;
 using iText.Kernel.Geom;
 using iText.Kernel.Pdf;
 using iText.Layout;
 using System;
+using System.Data;
 
 namespace GoldenFeedbacksMaker
 {
@@ -16,6 +18,17 @@ namespace GoldenFeedbacksMaker
             CreateBook();
         }
 
+        private static DataTable GetQuestions()
+        {
+            DataSet dsQuestions = null;
+            using (DBController dbController = new DBController())
+            {
+                dsQuestions = dbController.GetAllQuestions();
+            }
+
+            return dsQuestions.Tables[0];
+        }
+
         static void CreateBook()
         {
             var writer = new PdfWriter(DEST);
@@ -24,27 +37,32 @@ namespace GoldenFeedbacksMaker
 
             document.SetMargins(50, 50, 50, 50);
 
-            for (var i = 1; i < 10; i++)
+            DataTable dtQuestions = GetQuestions();
+
+            for (var i = 0; i < dtQuestions.Rows.Count; i++)
             {
                 Question q = new Question();
-                q.Number = i;
-                q.Content = "Thisis aquesti on :P";
+                q.Number = Convert.ToInt32(dtQuestions.Rows[i]["Number"]);
+                q.Content = dtQuestions.Rows[i]["Question"].ToString();
                 document.Add(q.GetQuestion());
 
                 Answer a = new Answer();
-                a.Number = 1;
-                a.Content = "The structures involved in snoring are the uvula and the soft palate. The muscles of soft palate and uvula are: tensor veli palatine, palatoglossus, palatopharyngeus, levator veli palatine and musculus uvulae.";
+                a.Number = Convert.ToInt32(dtQuestions.Rows[i]["Number"]);
+                a.Content = dtQuestions.Rows[i]["Answer"].ToString();
                 document.Add(a.GetAnswer());
-            }
 
-            Comment c = new Comment();
-            c.Title = "Just a title";
-            c.Content = "This is just a simple comment that should be in a comment section!";
-            document.Add(c.GetComment());
+                if (dtQuestions.Rows[i]["Title"] != DBNull.Value)
+                {
+                    Comment c = new Comment();
+                    c.Title = dtQuestions.Rows[i]["Title"].ToString();
+                    c.Content = dtQuestions.Rows[i]["Comment"].ToString();
+                    document.Add(c.GetComment());
+                }
+            }
 
             HeaderFooterHelper headerFooterHelper = new HeaderFooterHelper();
             Document doc = new Document(pdf);
-            headerFooterHelper.AddHeaderAndFooter(pdf, ref doc, "The Golden Feedbacks 2018 - Anatomy", 1, 3);
+            headerFooterHelper.AddHeaderAndFooter(pdf, ref doc, "The Golden Feedbacks 2018 - Anatomy", 1, 1);
 
             document.Close();
         }
